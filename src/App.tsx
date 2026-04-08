894231660df52cbf4b9e9815c53f61325eb377b4
Андрей Гуляйкин <a.gul@skbkontur.ru>
2026-04-08 20:25:27 +0500

    feat: initialize Vite + React + TypeScript + Mantine project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Stack, Title } from '@mantine/core';
+
+function App() {
+  return (
+    <Stack align="center" justify="center" h="100vh">
+      <Title>Hexlet AI</Title>
+      <Button>Get Started</Button>
+    </Stack>
+  );
+}
+
+export default App;
